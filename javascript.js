af96a794a1f2da76d2f0cbfe044c8b17571320fb
amaury-LC <Amaury.le.chaffotec@laposte.net>
2019-06-02 12:58:39 +0200

    look

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+
+function myFunction1(){
+
+    document.getElementById('titre').style.display = "none";
+    document.getElementById('wig').style.top = "20px";
+    document.getElementById('input1').style.display = "none";
+    
+
+    
+
+
+   
+
+
+}
