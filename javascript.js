dfd1299dd7584e9d605f88df0c5dae63de64f30e
amaury-LC <Amaury.le.chaffotec@laposte.net>
2019-06-02 14:13:55 +0200

    js

--- javascript.js
+++ javascript.js
@@ -3,8 +3,12 @@
 function myFunction1(){
 
     document.getElementById('titre').style.display = "none";
-    document.getElementById('wig').style.top = "20px";
+    document.getElementById('wig').style.top = "200px";
     document.getElementById('input1').style.display = "none";
+    document.getElementById('Wiggum').style.width = "70em";
+    document.getElementById('Wiggum').style.left = "15%";
+
+
     
 
     
